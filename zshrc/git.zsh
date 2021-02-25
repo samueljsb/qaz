7dbfaacd3179e36e1efe59c9c2151fa6b63e4790
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2021-02-25 11:50:00 +0000

    [git] Add alias to checkout main and pull
    This alias assumes the oh-my-zsh git plugin is installed as it uses some
    of the aliases defined therein. I type this many many times a day and
    often typo it, so I figured it should be an alias, even if this couples
    a few modules.

--- zshrc/git.zsh
+++ zshrc/git.zsh
@@ -1,3 +1,7 @@
 alias gpp='git push --set-upstream origin $(git branch --show-current)'
 alias gfu='git fu'
 alias glrbom='git pull --rebase origin master'
+
+# This assumes the oh-my-zsh git plugin is installed.
+# It's a shortcut for 'git checkout $(git_main_branch) && git pull'.
+alias gcml='gcm && gl'
