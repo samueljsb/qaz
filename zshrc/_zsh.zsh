a91c3533d77f8c2201152bbbbf5fefba8fe696a1
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2021-04-07 17:35:41 +0100

    [less] Disable line wrapping

--- zshrc/_zsh.zsh
+++ zshrc/_zsh.zsh
@@ -24,7 +24,7 @@ fi
 # Set pager options
 # -X is needed to fix a bug with the --quit-if-one-screen feature in old versions of less.
 # Unfortunately, it also breaks mouse-wheel support in less.
-export LESS='-RFX --tabs=4'
+export LESS='-SRFX --tabs=4'
 
 # Prompt
 RPROMPT="%B%*%b"
--- zshrc/less.zsh
+++ zshrc/less.zsh
@@ -1,3 +1,3 @@
 # The -X flag breaks mouse-wheel support, but it's unneeded for less >= 530.
 # We remove it from the default set in `_zsh.zsh` when we upgrade less.
-export LESS='-RF --tabs=4'
+export LESS='-SRF --tabs=4'
