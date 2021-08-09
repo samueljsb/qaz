c10e4f933a91f94e0aef8633ef60a5fd8c4dd978
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2021-08-09 13:01:31 +0100

    [GitHub] Add alias for common 'refresh' commands
    Prior to this change, I frequently ran `gcml && git gone && clg` to
    checkout main, refresh everything, and get some status reports.
    
    This change adds an alias that does all of that in one command.

--- zshrc/github.zsh
+++ zshrc/github.zsh
@@ -1,1 +1,13 @@
 alias clg='clear && gh pr status && git branch'
+
+# "Git refresh"
+# This assumes the oh-my-zsh git plugin is installed as well as my git config.
+# It's a shortcut for 'gcml && git gone && clg', which I use a lot.
+alias grf=': \
+  && git checkout $(git_main_branch) \
+  && git pull \
+  && git gone \
+  && clear \
+  && gh pr status \
+  && git branch \
+  && :'
