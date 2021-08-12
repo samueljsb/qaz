bfec92d9084b64e0fb09e88d6b89a4e080c854e5
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-08-12 13:26:52 +0100

    Add `last_upgraded` to the persisted settings

--- qaz/settings.py
+++ qaz/settings.py
@@ -1,7 +1,8 @@
+import datetime
 import json
 import logging
 from pathlib import Path
-from typing import Dict, TypedDict
+from typing import Dict, Optional, TypedDict
 
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ class ModuleSettings(TypedDict):
     """
 
     installed: bool  # is this module installed?
+    last_upgraded: str  # timestamp of last upgrade in ISO 8601 format
 
 
 class Settings(TypedDict):
@@ -44,6 +46,19 @@ def is_module_installed(name: str) -> bool:
     return name in settings["modules"]
 
 
+def get_last_upgraded_at(name: str) -> Optional[datetime.datetime]:
+    """
+    Retrieve the timestamp of the last update to this module.
+    """
+    settings = _load_settings_from_file()
+
+    if module_settings := settings["modules"].get(name):
+        if timestamp := module_settings.get("last_upgraded"):
+            return datetime.datetime.fromisoformat(timestamp)
+
+    return None
+
+
 # --------
 # Mutators
 # --------
@@ -69,7 +84,30 @@ def set_module_installed(name: str):
         module_settings["installed"] = True
     else:
         # If there are no settings for this module yet, create them.
-        config["modules"][name] = ModuleSettings(installed=True)
+        config["modules"][name] = ModuleSettings(
+            installed=True, last_upgraded=datetime.datetime.now().isoformat()
+        )
+
+    _save_settings_to_file(config)
+
+
+def record_module_upgraded(
+    name: str, upgraded_at: Optional[datetime.datetime] = None
+) -> None:
+    """
+    Mark a module as having been upgraded.
+
+    If no timestamp is passed in, the current time will be used.
+    """
+    upgraded_at = upgraded_at or datetime.datetime.now()
+
+    config = _load_settings_from_file()
+
+    if module_settings := config["modules"].get(name):
+        module_settings["last_upgraded"] = upgraded_at.isoformat()
+    else:
+        # If there are no settings for this module yet, we cannot mark it as upgraded.
+        raise ValueError(f"There is no information saved for the module '{name}'.")
 
     _save_settings_to_file(config)
 
