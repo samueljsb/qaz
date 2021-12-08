517f41079d9d6ea28c7a5b0e6f5550d846b69b61
Samuel Searles-Bryant <devel@samueljsb.co.uk>
2021-12-08 22:11:49 +0000

    Add function to create a new Python env in /tmp
    Prior to this change, I found myself frequently creating a new directory
    in /tmp, setting up a virtual environment, installing common tools, to
    test an idea quickly.
    
    This change adds a function to make that easier/quicker.

--- zshrc/python.zsh
+++ zshrc/python.zsh
@@ -2,3 +2,18 @@ export PYTHONSTARTUP=~/.config/pythonstartup.py
 
 # Django
 alias django='python manage.py'
+
+# Create a new directory with virtual environment in /tmp.
+function tmpvenv(){
+  # Create the new tmp directory.
+  # Exit early if this fails.
+  mkdir /tmp/$1 || return 1
+  cd /tmp/$1
+
+  python -m venv .venv
+  ./.venv/bin/pip install black isort flake8 mypy
+
+  . .venv/bin/activate
+
+  return 0
+}
