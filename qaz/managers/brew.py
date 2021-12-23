0aeb248b887550f9cb0dce971cc1bdde20f86c1f
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-12-23 18:15:03 +0000

    Implement pakcage manager class for Homebrew

--- qaz/managers/brew.py
+++ qaz/managers/brew.py
@@ -1,6 +1,64 @@
+import subprocess
 from typing import List
 
 from . import shell
+from .base import InstallResult, Manager, UpgradeResult
+
+
+class Homebrew(Manager):
+    @classmethod
+    def install(cls, name: str, /) -> InstallResult:
+        # Get the current version of this formula.
+        if version := cls._version(name):
+            return InstallResult.already_installed(version=version)
+
+        # Install.
+        proc = subprocess.run(
+            f"brew install {name}",
+            shell=True,
+            text=True,
+            capture_output=True,
+        )
+        if proc.returncode != 0:
+            return InstallResult.error(process=proc)
+
+        # Get the new version.
+        version = cls._version(name)
+        return InstallResult.installed(version=version, process=proc)
+
+    @classmethod
+    def upgrade(cls, name: str, /) -> UpgradeResult:
+        # Get the current version of this formula.
+        from_version = cls._version(name)
+
+        # Upgrade.
+        proc = subprocess.run(
+            f"brew upgrade {name}",
+            shell=True,
+            text=True,
+            capture_output=True,
+        )
+        if proc.returncode != 0:
+            return UpgradeResult.error(process=proc)
+
+        # Get the new version.
+        version = cls._version(name)
+        return UpgradeResult.upgraded(
+            from_version=from_version, to_version=version, process=proc
+        )
+
+    @staticmethod
+    def _version(name: str) -> str:
+        proc = subprocess.run(
+            f"brew list --versions {name}",
+            shell=True,
+            text=True,
+            capture_output=True,
+        )
+
+        if not proc.stdout:  # not installed
+            return ""
+        return proc.stdout.split()[-1]
 
 
 def install_or_upgrade_formula(formula: str):
