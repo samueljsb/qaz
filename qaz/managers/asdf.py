f35b1a4a35dc7e56f67fc3cae9ade9f94988da68
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-01-22 00:32:22 +0000

    Remove legacy asdf
    Prior to this change,
    
    This change

--- qaz/managers/asdf.py
+++ qaz/managers/asdf.py
@@ -64,38 +64,3 @@ class ASDF(Manager):
         versions = shell.capture(f"asdf list {self.plugin}").split()
         versions.sort(key=StrictVersion)
         return versions[-1]
-
-
-def install_or_upgrade_plugin(plugin: str) -> None:
-    shell.run("asdf update")
-
-    if plugin not in _get_installed_plugins():
-        shell.run(f"asdf plugin add {plugin}")
-    else:
-        shell.run(f"asdf plugin update {plugin}")
-
-    shell.run(f"asdf install {plugin} latest")
-    _set_latest(plugin)
-
-
-def _get_installed_plugins() -> list[str]:
-    return shell.capture("asdf plugin list").split()
-
-
-def _set_latest(plugin: str) -> None:
-    """
-    Set the version for a plugin to the latest installed version.
-    """
-    versions = _get_installed_versions(plugin)
-    shell.run(f"asdf global {plugin} {versions[-1]}")
-    shell.run(f"asdf reshim {plugin}")
-
-
-def _get_installed_versions(plugin: str) -> list[str]:
-    versions = shell.capture(f"asdf list {plugin}").split()
-
-    # Sort the versions.
-    # This is necessary to ensure '3.10.0' is later than '3.1.0'.
-    versions.sort(key=StrictVersion)
-
-    return versions
