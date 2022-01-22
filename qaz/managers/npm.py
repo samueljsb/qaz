68c129a7b714f6e1ccbda8a601b97502d39ac6c1
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-01-22 00:54:39 +0000

    add npm package

--- qaz/managers/npm.py
+++ qaz/managers/npm.py
@@ -1,8 +1,47 @@
 from __future__ import annotations
 
 import json
+import subprocess
 
 from . import shell
+from .base import Manager
+
+
+class NPM(Manager):
+    package: str
+
+    def __init__(self, package: str) -> None:
+        self.package = package
+
+    def install(self) -> tuple[str, subprocess.CompletedProcess[str] | None]:
+        # Install the package.
+        proc = subprocess.run(
+            f"npm install --global {self.package}",
+            shell=True,
+            text=True,
+            capture_output=True,
+        )
+        proc.check_returncode()
+
+        return self._version(), proc
+
+    def upgrade(self) -> tuple[str, str, subprocess.CompletedProcess[str]]:
+        from_version = self._version()
+
+        # Update the package.
+        proc = subprocess.run(
+            f"npm update --global {self.package}",
+            shell=True,
+            text=True,
+            capture_output=True,
+        )
+        proc.check_returncode()
+
+        return from_version, self._version(), proc
+
+    def _version(self) -> str:
+        data = json.loads(shell.capture("npm list --global --json"))
+        return data["dependencies"][self.package]["version"]
 
 
 def install_or_upgrade_package(package: str) -> None:
