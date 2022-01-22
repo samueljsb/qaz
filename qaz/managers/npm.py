71729fe03a7317ddefa04805f138f675af6f741d
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-01-22 00:56:01 +0000

    remove npm functions

--- qaz/managers/npm.py
+++ qaz/managers/npm.py
@@ -42,16 +42,3 @@ class NPM(Manager):
     def _version(self) -> str:
         data = json.loads(shell.capture("npm list --global --json"))
         return data["dependencies"][self.package]["version"]
-
-
-def install_or_upgrade_package(package: str) -> None:
-    if package not in _get_installed_packages():
-        shell.run(f"npm install --global {package}")
-    else:
-        shell.run(f"npm update --global {package}")
-
-
-def _get_installed_packages() -> list[str]:
-    output = shell.capture("npm list --global --json")
-    data = json.loads(output)
-    return [k for k in data.get("dependencies", {})]
