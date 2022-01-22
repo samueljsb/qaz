1173eaf4078016d4486015889c9be44eebda2ffd
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-01-22 01:30:04 +0000

    remove vscode func

--- qaz/managers/vs_code.py
+++ qaz/managers/vs_code.py
@@ -38,21 +38,6 @@ class VSCodeExtensions(Manager):
         return "", version, proc
 
 
-def install_extensions(extensions: Iterable[str]) -> None:
-    """
-    Install all currently uninstalled extensions.
-
-    VS Code extensions cannot be upgraded from the command line so we skip already
-    installed extensions for the sake of speed.
-    """
-    # If VS Code is not installed, don't try to install extensions.
-    if not _is_vs_code_installed():
-        return
-
-    for extension in set(extensions) - _get_installed_extensions():
-        shell.run(f"code --install-extension {extension}")
-
-
 def _is_vs_code_installed() -> bool:
     command = shell.capture("command -v code")
     return bool(command)
