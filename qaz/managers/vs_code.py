19172face5e5d8fcf9c20111d86dbbc40686707e
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-01-22 01:27:42 +0000

    add vscode manager

--- qaz/managers/vs_code.py
+++ qaz/managers/vs_code.py
@@ -1,8 +1,41 @@
 from __future__ import annotations
 
+import subprocess
 from collections.abc import Iterable, Set
 
 from . import shell
+from .base import Manager
+
+
+class VSCodeExtensions(Manager):
+    extensions: Iterable[str]
+
+    def __init__(self, extensions: Iterable[str]) -> None:
+        self.extensions = extensions
+
+    def install(self) -> tuple[str, None]:
+        # If VS Code is not installed, don't try to install extensions.
+        if not _is_vs_code_installed():
+            return "", None
+
+        # Install extensions.
+        # VS Code extensions cannot be upgraded from the command line so we skip already
+        # installed extensions.
+        for extension in set(self.extensions) - _get_installed_extensions():
+            proc = subprocess.run(
+                f"code --install-extension {extension}",
+                shell=True,
+                text=True,
+                capture_output=True,
+            )
+            proc.check_returncode()
+
+        return "", None
+
+    def upgrade(self) -> tuple[str, str, None]:
+        # Install any new extensions.
+        version, proc = self.install()
+        return "", version, proc
 
 
 def install_extensions(extensions: Iterable[str]) -> None:
