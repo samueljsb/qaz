0432613ae5997a32107bf5221d5f05975d3cbf72
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-02-07 21:46:34 +0000

    Make `clb` alias show commits
    Prior to this change, the `clb` alias cleared the screen and printed the
    branch name in big fancy letters at the top. I never use this.
    
    This change replaces it with a command to clear the screen and then
    print the log from the main brnch until the current head. I have this
    set up as a post-checkout hook at work and will use it much more often.

--- zshrc/figlet.zsh
+++ zshrc/figlet.zsh
@@ -1,3 +1,2 @@
 alias cls='clear && figlet -w `tput cols` `hostname -s`'
 alias cld='clear && figlet -w `tput cols` `basename $PWD`'
-alias clb='clear && figlet -w `tput cols` `current_branch`'
--- zshrc/git.zsh
+++ zshrc/git.zsh
@@ -2,6 +2,7 @@ alias gpp='git push --set-upstream origin $(git branch --show-current)'
 alias gfu='git fu'
 alias glrbom='git pull --rebase origin master'
 
-# This assumes the oh-my-zsh git plugin is installed.
-# It's a shortcut for 'git checkout $(git_main_branch) && git pull'.
+# These assume the oh-my-zsh git plugin is installed.
+# A shortcut for 'git checkout $(git_main_branch) && git pull'.
 alias gcml='gcm && gl'
+alias clb='clear; PAGER= git log $(git_main_branch)..HEAD --oneline'
