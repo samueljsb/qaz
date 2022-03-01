6ed391e359bc8d310c92f642f5b1f06c92389246
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-03-01 18:38:08 +0000

    [fzf] Add alias for fixing up with `-n` flag
    Prior to this change, we had a useful command to use fzf to find a
    commit to fixup. However, we couldn't skip the pre-commit hook.
    
    This change adds an alias that allows us to skip the pre-commit hook.

--- zshrc/fzf.zsh
+++ zshrc/fzf.zsh
@@ -1,2 +1,3 @@
 # git fixup alias to select the commit to fix up
 alias gfu="git log --pretty=format:'%h %s' --no-merges $(git_main_branch).. | fzf | cut -c -7 | xargs -o git commit --fixup"
+alias gfun="git log --pretty=format:'%h %s' --no-merges $(git_main_branch).. | fzf | cut -c -7 | xargs -o git commit -n --fixup"
