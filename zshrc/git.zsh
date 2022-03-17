e6b5d088c08ccca94d52444244f10b605f5068fb
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-03-17 16:34:40 +0000

    [git] Add function to grab the 'co-author' tag for GitHub

--- zshrc/git.zsh
+++ zshrc/git.zsh
@@ -5,3 +5,7 @@ alias glrbom='git pull --rebase origin master'
 # A shortcut for 'git checkout $(git_main_branch) && git pull'.
 alias gcml='gcm && gl'
 alias clb='clear; PAGER= git log $(git_main_branch)..HEAD --oneline'
+
+function co-author(){
+  git log --author=$1 | grep -m 1 $1 | gsed 's/Author/Co-authored-by/'
+}
