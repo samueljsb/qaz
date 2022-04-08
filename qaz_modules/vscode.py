71f26440987491509e6b8b210976fb777f8e4a9e
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-04-08 21:00:07 +0100

    [VS Code] Remove extensions I don't use
    I don't use these extensions so they're just bloat (and some of them
    don't seem to work).

--- qaz_modules/vscode.py
+++ qaz_modules/vscode.py
@@ -31,42 +31,20 @@ class VSCode(Module):
         "mikestead.dotenv",
         "naumovs.color-highlight",
         "PKief.material-icon-theme",
-        "pnp.polacode",
         "stkb.rewrap",
         "swyphcosmo.spellchecker",
-        "VisualStudioExptTeam.vscodeintellicode",
         # Git
         "codezombiech.gitignore",
         "knisterpeter.vscode-github",
-        "npxms.hide-gitignored",
         "sidneys1.gitconfig",
         "waderyan.gitblame",
-        # NodeJS
-        "dbaeumer.vscode-eslint",
-        "esbenp.prettier-vscode",
-        "numso.prettier-standard-vscode",
         # Python
-        "freakypie.code-python-isort",
         "lextudio.restructuredtext",
         "ms-python.python",
         "ms-python.vscode-pylance",
-        "wholroyd.jinja",
-        # Ruby
-        "rebornix.ruby",
-        "sissel.shopify-liquid",
-        "wingrunr21.vscode-ruby",
-        # Rust
-        "rust-lang.rust",
-        # TOML
-        "bungcip.better-toml",
-        # Misc.
+        # Other
         "DavidAnson.vscode-markdownlint",
-        "mrmlnc.vscode-apache",
         "ms-azuretools.vscode-docker",
-        "syler.sass-indented",
-        "torn4dom4n.latex-support",
-        "wholroyd.jinja",
-        "william-voyek.vscode-nginx",
     ]
 
     def install_action(self) -> None:
