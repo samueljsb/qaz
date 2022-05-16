456af268680142d8743a580d787dcfe1923884bc
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-05-17 00:32:01 +0100

    Replace uses of OMZ's `git_main_branch` with local replacement

--- zshrc/fzf.zsh
+++ zshrc/fzf.zsh
@@ -1,6 +1,6 @@
 # git fixup alias to select the commit to fix up
-alias gfu='git log --pretty=format:"%h %s" --no-merges $(git_main_branch).. | fzf | cut -c -7 | xargs -o git commit --fixup'
-alias gfun='git log --pretty=format:"%h %s" --no-merges $(git_main_branch).. | fzf | cut -c -7 | xargs -o git commit -n --fixup'
+alias gfu='git log --pretty=format:"%h %s" --no-merges $(_git_main_branch).. | fzf | cut -c -7 | xargs -o git commit --fixup'
+alias gfun='git log --pretty=format:"%h %s" --no-merges $(_git_main_branch).. | fzf | cut -c -7 | xargs -o git commit -n --fixup'
 
 # Use fzf to find a file in a git repo and open it in the editor.
 function ef(){
--- zshrc/git.zsh
+++ zshrc/git.zsh
@@ -1,10 +1,19 @@
+# Find the main branch of this repo.
+_git_main_branch () {
+  git branch 2> /dev/null \
+  | grep -o -m 1 \
+    -e ' main$' \
+    -e ' master$' \
+  | xargs \
+  || return
+}
+
+
 alias gpp='git push --set-upstream origin $(git branch --show-current)'
 alias glrbom='git pull --rebase origin master'
 
-# These assume the oh-my-zsh git plugin is installed.
-# A shortcut for 'git checkout $(git_main_branch) && git pull'.
 alias gcml='gcm && gl'
-alias clb='clear; PAGER= git log $(git_main_branch)..HEAD --oneline'
+alias clb='clear; PAGER= git log $(_git_main_branch)..HEAD --oneline'
 
 function co-author(){
   git log --author=$1 | grep -m 1 $1 | gsed 's/Author/Co-authored-by/'
