6db3a457205498288737ba7672936365f0e09321
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-05-17 01:21:52 +0100

    Refactor to extract useful helper alias

--- zshrc/fzf.zsh
+++ zshrc/fzf.zsh
@@ -1,6 +1,8 @@
+alias _fzf_commit_sha='git log --pretty=format:"%h %s" --no-merges $(_git_main_branch).. | fzf | cut -c -7'
+
 # git fixup alias to select the commit to fix up
-alias gfu='git log --pretty=format:"%h %s" --no-merges $(_git_main_branch).. | fzf | cut -c -7 | xargs -o git commit --fixup'
-alias gfun='git log --pretty=format:"%h %s" --no-merges $(_git_main_branch).. | fzf | cut -c -7 | xargs -o git commit -n --fixup'
+alias gfu='_fzf_commit_sha | xargs -o git commit --fixup'
+alias gfun='_fzf_commit_sha | xargs -o git commit -n --fixup'
 
 # Use fzf to find a file in a git repo and open it in the editor.
 function ef(){
