b528e204bb57c979fe7023a61836180087911dfa
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2022-05-17 03:17:26 +0100

    Make Python a bundle

--- qaz_modules/python.py
+++ qaz_modules/python.py
@@ -1,24 +1,31 @@
 from __future__ import annotations
 
 from qaz import managers
+from qaz.modules.base import Bundle
 from qaz.modules.base import Module
 from qaz.modules.registry import registry
 from qaz.utils import shell
 
 
 @registry.register
-class Python(Module):
+class Python(Bundle):
     name = "Python"
 
+    managers = (
+        managers.BrewFormula("pre-commit"),
+        managers.BrewFormula("virtualenv"),
+    )
+
     # Configuration files
-    zshrc_file = "python.zsh"
-    symlinks = {".pypirc": "~", "pythonstartup.py": "~/.config/", ".pdbrc.py": "~"}
-
-
-@registry.register
-class PreCommit(Module):
-    name = "pre-commit"
-    manager = managers.BrewFormula("pre-commit")
+    zshrc_files = (
+        "python.zsh",
+        "virtualenv.zsh",
+    )
+    symlinks = {
+        ".pypirc": "~",
+        "pythonstartup.py": "~/.config/",
+        ".pdbrc.py": "~",
+    }
 
     def install_action(self) -> None:
         shell.run("pre-commit", "init-templatedir", "~/.git-template")
@@ -31,12 +38,3 @@ class PreCommit(Module):
 class PythonLauncher(Module):
     name = "py"
     manager = managers.BrewFormula("python-launcher")
-
-
-@registry.register
-class Virtualenv(Module):
-    name = "virtualenv"
-    manager = managers.BrewFormula("virtualenv")
-
-    # Configuration files
-    zshrc_file = "virtualenv.zsh"
