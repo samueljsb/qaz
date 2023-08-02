00a274ff665b6503b238c982d6e75cddf829e2d7
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-08-02 22:23:22 +0100

    [fzf] Function for switching git branches

--- zshrc/fzf.zsh
+++ zshrc/fzf.zsh
@@ -4,6 +4,10 @@ function gfu(){
   git commit --fixup $commit_sha $@
 }
 
+function gswf(){
+  git branch --format='%(refname:short)' | fzf --select-1 --query=$@ | xargs git switch
+}
+
 # Use fzf to find a file in a git repo and open it in the editor.
 function ef(){
   git ls-files | fzf --select-1 --multi --print0 --query=$@ | xargs -0 $EDITOR
